aeef93d1e2ab4084540c4bfee5754547a6ef9517
TheSaiTeja <iamsaiteja2411@gmail.com>
2024-12-13 12:53:05 +0530

    add backend folder and files

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+}
+
+const express = require("express");
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(` App listening on port ${port}`);
+});
