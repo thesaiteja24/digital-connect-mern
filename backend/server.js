2092f9dca4c950c094566aee1112a035842f1428
TheSaiTeja <iamsaiteja2411@gmail.com>
2024-12-13 13:33:02 +0530

    establish mongo database connection

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,16 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const app = express();
 const port = process.env.PORT;
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+// Database Connection
+const dbUrl = process.env.ATLAS_DB_URI;
+mongoose
+  .connect(dbUrl)
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => console.error("Failed to connect to DB:", err));
+
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
